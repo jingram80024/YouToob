10f8b46d3ad04815cf42adbcb13f1fc44e38577b
Jonathan Ingram <jingram6.0223@yahoo.com>
2023-02-02 17:01:04 -0500

    Restructure for iFrame API [issue 01]

--- docs/main.js
+++ docs/main.js
@@ -2,19 +2,51 @@
 /* DOM element declarations
 -----------------------------------------*/
 const form = document.getElementById("url_field");
+/* Load YouTube IFrame API
+-----------------------------------------*/
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-/* Utility function for extracting YouTube ID
+var player;
+function onYouTubePlayerAPIReady() {
+    player = new YT.Player('player', {
+      height: '390',
+      width: '640',
+      videoId: 'muJK8Rx-Yhc',
+      playerVars:{
+        playsinline: 1,
+        controls: 1,
+        modestbranding: 1
+      }
+    });
+}
+
+/* Utility functions for extracting YouTube ID or Playlist ID
 -----------------------------------------*/
 
 function getVideoID(url){
-    const regex = /(\/|%3|v=|vi=)([0-9A-z-_]{11})([%#?&]|$)/;
-    let videoID = url.split(regex)[2];
+    const regex_vid = /(\/|%3|v=|vi=)([0-9A-z-_]{11})([%#?&]|$)/;
+    let videoID = url.split(regex_vid)[2];
     return videoID;
+}
+
+function getListID(url){
+    const regex_lst = /(list=)(.*?)([%#?&]|$)/gm;
+    let listID = url.split(regex_lst)[2];
+    return listID;
 }
 
 /* "Play" button action
 -----------------------------------------*/
-
+function logPlayerStatus(){
+    state = player.getPlayerState();
+    url = player.getVideoUrl();
+    playlist = player.getPlaylist();
+    index = player.getPlaylistIndex();
+    console.log('state: ', state, '| url: ', url, '| playlist: ', playlist, '| index: ', index);
+}
 form.addEventListener("keydown",function(event){
     if (event.key === "Enter"){
         getURL();
@@ -24,15 +56,38 @@ form.addEventListener("keydown",function(event){
 function getURL(){
     const rawURL = form.value;
     if (rawURL == null || rawURL == '' || rawURL == 'http://localhost:8080/'){return;}
+    if (rawURL.includes("list")){
+        const listID = getListID(rawURL);
+        updatePlayerList(listID);
+        return;
+    }
     const videoID = getVideoID(rawURL);
-    updatePlayerURL(videoID)
+    updatePlayerVideo(videoID);
 }
 
-function updatePlayerURL(videoID){
-    const repeatURL = "https://www.youtube.com/embed/" + videoID + "?playlist=" + videoID + "&loop=1";
-    const player = document.getElementById("ytplayer");
-    player.src = repeatURL;
-    document.getElementById("url_field").value = "";
+function updatePlayerList(listID){
+    console.log('cueing ', listID);
+    player.destroy();
+    player = new YT.Player('player', {
+      height: '390',
+      width: '640',
+      playerVars:{
+        playsinline: 1,
+      	listType: 'playlist',
+        list: listID,
+        modestbranding: 1,
+      }
+    });
+}
+
+function updatePlayerVideo(videoID){
+    //const repeatURL = "https://www.youtube.com/embed/" + videoID + "?playlist=" + videoID + "&loop=1";
+    //const player = document.getElementById("ytplayer");
+    //player.src = repeatURL;
+    //document.getElementById("url_field").value = "";
+    //
+    console.log("cueing video",videoID);
+    player.loadVideoById({videoId: videoID});
 }
 
 /* Adding favorites
@@ -55,7 +110,7 @@ function buildPage(videoID){
     fav.id = videoID;
     fav.src = thumbURL(videoID);
     fav.onclick = function(event) {
-        updatePlayerURL(this.id);
+        updatePlayerVideo(this.id);
     }
     container = document.createElement('div');
     container.classList.add('container');
@@ -147,16 +202,3 @@ window.addEventListener('load', () => {
         buildPage(ID);
     }
 });
-
-
-
-// TO DO:
-
-// - **BEFORE PUSHING TO SOURCE** read on how to make API key a protected variable in the GitHub pages env.
-//      --> this actually wont work because no mater what the client will need the key and this is a security issue....
-//      so either you need to host a server at home that gets the request from GitHub and then returns the title, or just don't.....me no dev
-// - Figure out how to count # of plays and store to local storage.
-// - CSS styling of the cards into adjacent grid pattern that can flex based on screen size
-// - Think about how to style site. When favorites are added, the canvas with particles doesn't reach that far.
-// - Determine what happens if API quota is met. Am I charged? or is request denied?
-// - Maybe add functionality to send api requests for playlists, searches, etc?
